6861f096a3f7232a7461a1ac06204770bc750e5c
Matt DeBoard <matt.deboard@gmail.com>
2026-03-01 15:04:16 -0600

    Replace another really long css class with classNames

--- frontend/src/components/TranslationInput.tsx
+++ frontend/src/components/TranslationInput.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames'
 import { useEffect, useState } from 'react'
 import { getLanguageName, useI18n } from '../i18n'
 import { LanguageCode } from '../types/alignment'
@@ -121,7 +122,25 @@ export function TranslationInput({
       <div className="fixed bottom-6 right-6 z-50">
         <button
           onClick={() => setIsExpanded(true)}
-          className="bg-teal-700 hover:bg-teal-800 text-white w-14 h-14 rounded-full shadow-lg flex items-center justify-center transition-all duration-300 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
+          className={classNames(
+            'bg-teal-700',
+            'hover:bg-teal-800',
+            'text-white',
+            'w-14',
+            'h-14',
+            'rounded-full',
+            'shadow-lg',
+            'flex',
+            'items-center',
+            'justify-center',
+            'transition-all',
+            'duration-300',
+            'hover:scale-105',
+            'focus:outline-none',
+            'focus:ring-2',
+            'focus:ring-teal-500',
+            'focus:ring-offset-2'
+          )}
           aria-label={t('mode.analyze_new_text')}
           type="button"
         >
